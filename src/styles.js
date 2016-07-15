a50170aca4b82dfff78c392313305dbaaf5e23b5
Ricard Sole <ricardsolecasas@gmail.com>
2016-07-15 10:49:28 +0200

    Drops reactcss and moves to transpiled folder

--- src/styles.js
+++ src/styles.js
@@ -1,18 +1,20 @@
-import reactCSS from 'reactcss'
+const content = {
+  position: 'relative',
+  zIndex: 11
+}
 
-export default reactCSS({
-  default: {
-    overlay: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      zIndex: 10
-    },
-    trigger: {
-      position: 'relative',
-      zIndex: 11
-    }
-  }
-})
+const overlay = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  zIndex: 10
+}
+
+const trigger = {
+  position: 'relative',
+  zIndex: 11
+}
+
+export {content, overlay, trigger}
